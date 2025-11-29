f557ed03291060d9e55ed0b64ce6d877e0a4cb33
Kvn-ox <kevin.lopes@gec.inatel.br>
2025-11-29 10:17:58 -0300

    Refactor Utensilio class to include category

--- Projeto poo/src/Utensilio.java
+++ Projeto poo/src/Utensilio.java
@@ -3,30 +3,20 @@ package Mercado.Utensilios;
 import Mercado.Produto;
 
 public class Utensilio extends Produto {
-    private String marca;
 
-    public Utensilio(int quantidade, String nome, double preco, String marca, String tipo) {
-        super(quantidade, nome, preco, tipo);
+    private String marca;
+    private String categoria;
+
+    public Utensilio(int quantidade, String nome, double preco, String marca, String categoria) {
+        super(quantidade, nome, preco, categoria);
         this.marca = marca;
+        this.categoria = categoria;
     }
+
+    public String getMarca() { return marca; }
 
     @Override
     public void mostrar() {
-        System.out.printf("[Utensílio] %s - Marca: %s - Preço: R$ %.2f - Qtd: %d%n",
-                nome, marca, preco, quantidade);
-    }
-
-    @Override
-    public double getPreco() {
-        return preco;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    @Override
-    public void Addproduto() {
-        System.out.println("Adicionando Utensilio");
+        System.out.println(getNome() + " | " + marca + " | R$ " + getPreco());
     }
 }
